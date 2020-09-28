8b28c4bcabb474e76fdf8fc338b5d437480d4b89
Tatico <huhy1099@foxmail.com>
2020-09-28 13:55:56 +0800

    init

---
+++ src/test/java/indi/huhy/template/Main.java
@@ -0,0 +1,16 @@
+package indi.huhy.template;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author huhy
+ * @version 1.0
+ * @date 2020/9/28 13:52
+ */
+@Slf4j
+public class Main {
+
+    public static void main(String[] args) {
+        log.info("123123");
+    }
+}
